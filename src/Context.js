3b79ca745ba065bd8f6c5e28483146232d9b80e7
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-06-18 16:29:44 -0500

    Finishing touches before migrating data to Contentful

--- src/Context.js
+++ src/Context.js
@@ -9,17 +9,34 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: "all",
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
-
 
     componentDidMount() {
         let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.feature === true);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        //
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
         this.setState({
-            rooms, featuredRooms, sortedRooms: rooms, loading: false
-        })
-        console.log(rooms);
+          rooms,
+          featuredRooms,
+          sortedRooms: rooms,
+          loading: false,
+          //
+          price: maxPrice,
+          maxPrice,
+          maxSize
+        });
     }
 
     formatData(items) {
@@ -33,11 +50,67 @@ class RoomProvider extends Component {
         return tempItems;
     }
 
+
+    getRoom = slug => {
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find(room => room.slug === slug);
+        return room;
+      };
+
+
+      handleChange = event => {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        console.log(name, value);
+    
+        this.setState(
+          {
+            [name]: value
+          },
+          this.filterRooms
+        );
+      };
+
+
+      filterRooms = () => {
+        let {
+          rooms,
+          type,
+          capacity,
+          price,
+          minSize,
+          maxSize,
+          breakfast,
+          pets
+        } = this.state;
+        let tempRooms = [...rooms];
+        capacity = parseInt(capacity);
+        price = parseInt(price);
+        if (type !== "all") {
+          tempRooms = tempRooms.filter(room => room.type === type);
+        }
+        if (capacity !== 1) {
+          tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+        }
+        tempRooms = tempRooms.filter(room => room.price <= price);
+        tempRooms = tempRooms.filter(
+          room => room.size >= minSize && room.size <= maxSize
+        );
+        if (breakfast) {
+          tempRooms = tempRooms.filter(room => room.breakfast === true);
+        }
+        if (pets) {
+          tempRooms = tempRooms.filter(room => room.pets === true);
+        }
+        this.setState({
+          sortedRooms: tempRooms
+        });
+      };
     render() {
         return (
-            <RoomContext.Provider value={{...this.state}}>
+            <RoomContext.Provider value={{...this.state, getRoom: this.getRoom, handleChange: this.handleChange}}>
                 {this.props.children}
-
             </RoomContext.Provider>
         );
     }
@@ -46,3 +119,13 @@ class RoomProvider extends Component {
 const RoomConsumer = RoomContext.Consumer;
 
 export { RoomProvider, RoomConsumer, RoomContext };
+
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props) {
+      return (
+        <RoomConsumer>
+          {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+      );
+    };
+  }

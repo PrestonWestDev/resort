a958cd4a86868a14fa0c8afe0520eb7450398e14
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-06-21 15:23:40 -0500

    Update FeaturedRooms.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,7 +5,7 @@ import Title from './Title';
 const FeaturedRooms = ({rooms}) => {
 
     const featuredRooms = rooms.filter((room, index) => 
-    (room.featured == true && rooms.indexOf(room)) === index)
+        (room.featured == true && rooms.indexOf(room)) === index)
     .map(featuredRoom => {
         return <Room key={featuredRoom.id} room={featuredRoom}></Room> 
     });
